cf62f55fc87931172fbf0b4c12051016fae5b1ac
Sabbir486 <australiasabbir355@gmail.com>
2025-07-17 05:07:57 +0600

    new

--- Lecture-01/01.py
+++ Lecture-01/01.py
@@ -1,1 +1,14 @@
-print("Hello World, I am Back")
+print("Hello World!", "I am Back")
+
+name = "Sabbir"
+age = 25
+price = 25.99
+
+print("My name is: ",name)
+print("My age is: ",age, price)
+
+
+# Data Types
+print(type(name))
+print(type(age))
+print(type(price))
---
+++ Lecture-01/02_Sum.py
@@ -0,0 +1,8 @@
+a = 5
+b = 3
+
+sum = a+b
+print(sum)
+
+diff = a-b
+print(diff)
