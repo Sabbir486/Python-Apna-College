8263fabcba1f55384f01e798268b5b47ebe3060a
Sabbir486 <australiasabbir355@gmail.com>
2025-07-17 05:20:52 +0600

    modified

--- Lecture-01/02_Sum.py
+++ Lecture-01/02_Sum.py
@@ -6,3 +6,17 @@ print(sum)
 
 diff = a-b
 print(diff)
+
+
+# Logical Operator->not, and, or
+
+print(not False)
+a=50
+b=20
+print(not (a<b))
+
+val1 = True
+val2 = True
+
+print("And Operator: ", val1 and val2)
+print("Or Operator: ", val1 or val2)
