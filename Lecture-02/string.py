70ff573ef6cdb81d5ab73684be9c116de42b2bbd
Sabbir486 <australiasabbir355@gmail.com>
2025-07-20 04:32:10 +0600

    basic

---
+++ Lecture-02/string.py
@@ -0,0 +1,32 @@
+str1 = "This is Sabbir."
+str2 = 'This is Sabbir.'
+
+str3 = "This is Sabbir's Life."
+
+print(str1)
+print(str2)
+print(str3)
+print()
+
+str4 = "This is Sabbir.\nI am a student."
+print(str4)
+
+# Operations On Strings
+
+# Concatenation
+
+st1 = "Hello"
+st2 = "World"
+print(st1+st2)
+
+# Length Of String
+
+st3 = "HelloSabbir"
+print(len(st3))
+
+# Indexing
+
+st4 = "King of Jungle"
+print(st4[0])      # First character
+print(st4[-1])     # Last character
+print(st4[5:10])    # Characters from index 5 to 9 (Slicing)
