0ecc17d52400032e1ce2d50e483154c25e283688
Sabbir486 <australiasabbir355@gmail.com>
2025-07-20 04:54:19 +0600

    Functions

---
+++ Lecture-02/string_opeeration.py
@@ -0,0 +1,18 @@
+str = "i am a am Coder."
+print(str.endswith("r."))
+print()
+
+print(str.capitalize())
+print(str) #Change only one time
+print()
+
+print(str.replace("a","R"))
+print()
+
+print(str.find("am")) #Return 1st index of 1st Occur
+print()
+
+print(str.count("a"))
+print()
+
+ 
