8b7f6eed908069123d2ff2e646c0ea3ad4cfb9bc
Sabbir486 <australiasabbir355@gmail.com>
2025-07-23 17:53:55 +0600

    List Method

---
+++ Lecture-03/02_List_Method.py
@@ -0,0 +1,22 @@
+list = [2,1,3]
+
+list.append(5) #Add element at last
+print(list)
+
+list.sort() #Sort in ascending order
+print(list)
+
+list.sort(reverse=True) #Sort in descending order
+print(list)
+
+list.reverse() #Reverse the main List
+print(list)
+
+list.insert(2, 10)
+print(list)
+
+list.remove(2) #Remove first occurrence of elements
+print(list)
+
+list.pop(1) #Remove element at Index
+print(list)
