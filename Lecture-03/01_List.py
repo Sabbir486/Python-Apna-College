1e274530b1f6e7e6cb9fd7f32ffda7a480dd7f68
Sabbir486 <australiasabbir355@gmail.com>
2025-07-23 17:12:38 +0600

    List

---
+++ Lecture-03/01_List.py
@@ -0,0 +1,24 @@
+# mark1 = 94.5
+# mark2 = 90.5
+# mark3 = 84.5
+
+
+marks = [94.5, 90.5, 84.5, 80.5, 75.5]
+print(marks)
+print(type(marks))
+print()
+
+print(marks[0])
+print(marks[3])
+print()
+
+print(len(marks))
+print()
+
+# Different data types in a single list
+student = ["karan", 95.5, 17, "Delhi"]
+print(student)
+print()
+
+student[0] = "Sabbir"
+print(student)
