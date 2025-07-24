87065ad0a7dc142f51f81bddfdc003b7b60b9032
Sabbir486 <australiasabbir355@gmail.com>
2025-07-24 22:01:23 +0600

    Tupple Method

---
+++ Lecture-03/03_Tuples.py
@@ -0,0 +1,16 @@
+tup = (2,1,3,4,1)
+print(type(tup))
+print()
+# Tupple is immutable
+
+# Single Value Tupple declaration ->
+
+tup1 = (3,)
+print(tup1)
+print()
+
+# Tupple Methods
+
+print(tup.index(1)) #Give the first index of that element
+
+print(tup.count(1)) #Give the number of that element
