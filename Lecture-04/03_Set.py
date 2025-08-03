c7a26ada8ab696edaa88faef00f81017f37bccbe
Sabbir486 <australiasabbir355@gmail.com>
2025-08-03 23:10:16 +0600

    Basic Set

--- Lecture-04/03_Set.py
+++ Lecture-04/03_Set.py
@@ -1,1 +1,19 @@
-print("Sabbir Ahmed")
+# Each element in set is immutable
+# Set is unordered
+
+collection = {1,2,3,4, "hello", "world"}  #Ignore duplicate values
+
+print(collection)
+print(type(collection))
+print(len(collection))  #Number of items
+print()
+
+
+# Creating empty set
+
+null = {} #Empty Dictionary
+print(type(null))
+
+null_set = set()  #Empty Set
+print(null_set)
+print(type(null_set))
