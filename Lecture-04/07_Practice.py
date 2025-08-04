562929d86f6d4d37f6593938131e932ee8e2f0c2
Sabbir486 <australiasabbir355@gmail.com>
2025-08-04 16:42:52 +0600

    Modified

--- Lecture-04/07_Practice.py
+++ Lecture-04/07_Practice.py
@@ -1,1 +1,14 @@
-print("Sabbir Ahmed")
+# Question3
+
+sub = {}
+
+sub1 = {"phy" : 90}
+sub.update(sub1)
+
+sub2 = {"che" : 95}
+sub.update(sub2)
+
+sub3 = {"math" : 100}
+sub.update(sub3)
+
+print(sub)
