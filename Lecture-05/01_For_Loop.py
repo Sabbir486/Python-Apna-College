ab3f2b7738d23f314331bb581cf3db82293a740e
Sabbir486 <australiasabbir355@gmail.com>
2025-08-27 04:40:58 +0600

    For Loop

--- Lecture-05/01_For_Loop.py
+++ Lecture-05/01_For_Loop.py
@@ -1,1 +1,20 @@
-print("My name is Sabbir")
+# For Loop
+
+list = [1,2,3,4,5, 'Sa', 'Ra']
+
+for num in list:
+    print(num)
+
+print ("End Loop")
+
+# Applying on Tupple:
+
+tup = (1,2,3,4,5,6,7,8)
+
+for val in tup:
+    print(val)
+
+else:
+    print("End")
+
+print ("End Loop")
